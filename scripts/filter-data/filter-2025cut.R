91d9eab09a66da9d063a063498a97c219cf650ee
Katherine Hébert <katherine.ahebert@gmail.com>
2026-05-12 16:03:54 -0400

    get max date of observations in the pq file

--- scripts/filter-data/filter-2025cut.R
+++ scripts/filter-data/filter-2025cut.R
@@ -3,7 +3,9 @@ library(data.table)
 
 dat = data.table::fread("data/heavy/inaturalist-canada-5-6Dec2025-nousergeopriv/inaturalist-canada-5-observations-no-usergeopriv.csv",
                         select = c("observed_on", "observed_on_string"))
-                        
+# get latest date
+dat$observed_on |> max(na.rm = T)
+
 dat_2025 = dat[grep("2025", dat$observed_on_string),]
 row_to_keep = grep("2025", dat$observed_on_string)
 
