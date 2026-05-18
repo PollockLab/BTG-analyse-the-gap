7e15bb94451a49f64c74e7b9f858366386a3707e
Katherine Hébert <katherine.ahebert@gmail.com>
2026-05-18 16:37:24 -0400

    cut parquet into new subsets (blitz the gap months)

--- scripts/filter-data/make-parquet.R
+++ scripts/filter-data/make-parquet.R
@@ -20,7 +20,8 @@ df = arrow::read_parquet("data/heavy/BTG-data/inaturalist-canada-dec2025.parquet
 # format to be lighter and to get a "year" column
 df = df |>  
   dplyr::filter(captive_cultivated == FALSE,
-                place_country_name == "Canada") |>
+                place_country_name == "Canada",
+                quality_grade != "casual") |>
   select(c(observed_on,
            user_login, 
            quality_grade, 
@@ -39,11 +40,15 @@ arrow::write_parquet(df, "data/heavy/BTG-data/inaturalist-canada-dec2025_smaller
 # read in pq file
 df = arrow::read_parquet("data/heavy/BTG-data/inaturalist-canada-dec2025_smaller.parquet")
 
+## 1. Make 2025 subset ---------------------------------------------------------
+
 # filter to 2025
 df_2025 = df |>
   dplyr::filter(year == "2025") |>
   dplyr::filter(quality_grade != "casual") # remove obs that are not verifiable
 arrow::write_parquet(df_2025, "data/heavy/BTG-data/inaturalist-canada-dec2025_subset2025.parquet")
+
+## 2. Make BTG (APR-OCT) subset ------------------------------------------------
 
 # filter to APR 1 TO OCT 1
 df_APROCT = df_2025 |>
@@ -52,6 +57,17 @@ df_APROCT = df_2025 |>
 df_APROCT = df_APROCT[-which(df_APROCT$month == "10" & df_APROCT$day != "01"),]
 arrow::write_parquet(df_APROCT, "data/heavy/BTG-data/inaturalist-canada-dec2025_APR1OCT1.parquet")
 
+## 3. Make BTG (JUN-OCT) subset ------------------------------------------------
+
 # filter to JUNE 1 TO OCT 1
 df_BTG = df_APROCT |> dplyr::filter(month != "04") 
 arrow::write_parquet(df_BTG, "data/heavy/BTG-data/inaturalist-canada-dec2025_JUN1OCT1.parquet")
+
+## 4. Make pre-BTG subset (everything before JUN 1 2025) -----------------------
+
+df.preBTG = df |>
+  mutate_at(vars(year, month, day), as.numeric) |>
+  # make the date into a number we can filter from
+  mutate(date_num = year*10000 + month*100 + day) |>
+  filter(date_num < 20250601) 
+arrow::write_parquet(df.preBTG, "data/heavy/BTG-data/inaturalist-canada-dec2025_PRE_JUN12025.parquet")
