fd95492007280ea54ecfd9ebd9050f05746bf33b
Katherine Hébert <katherine.ahebert@gmail.com>
2026-06-12 14:34:08 -0400

    update with the inat 'leaves' numbers

--- scripts_ms/11_numberofobs_gains_taxagroups.R
+++ scripts_ms/11_numberofobs_gains_taxagroups.R
@@ -117,14 +117,16 @@ btg_temp = btg_temp |>
          "diffperc" = 100*(n_min100.post-n_min100.pre)/n_min100.post) |>
   mutate("Percentage" = paste0(round(diffperc, digits = 0), "%"),
          "Species" = paste0(diff, " sp."))
+
+# manually adding total species counts from inat.ca 'leaves'
+# on june 12, 2026
+btg_temp$total_sp = c(574, 54, 2387, 1398, 759, 891, 9064, 19459, 215, 948, 9087, 519, 56)
+
+# fix and order taxon names
 btg_temp$iconic_taxon_name = gsub("Animalia", "Other Animalia", btg_temp$iconic_taxon_name)
 btg_temp$iconic_taxon_name = factor(btg_temp$iconic_taxon_name,
-                                    levels = btg_temp$iconic_taxon_name[order(btg_temp$n_min100.post/btg_temp$n_total.post)])
+                                    levels = btg_temp$iconic_taxon_name[order(btg_temp$n_min100.post/btg_temp$total_sp)])
 
-# manually adding total species counts from inat.ca filtering all observations 
-# on june 12, 2026 (as not all taxa in the parquet are species level)
-# taking the Other Animalia count from the inat parquet since it is a catch-all group
-btg_temp$total_sp = c(574, 54, 1659, 1398, 759, 891, 9064, 19460, 215, 950, 9086, 519, 65)
 
 # Relative gain with n species labels ------------------------------------------
 
@@ -134,158 +136,94 @@ ggplot(data = btg_temp,
                x = 100),
            stat = "identity", fill = "black") +
   geom_bar(aes(y = iconic_taxon_name, 
-               x = 100*n_min100.pre/n_total.post),
+               x = 100*n_min100.pre/total_sp),
            stat = "identity", alpha = 1) +
   geom_bar(aes(y = iconic_taxon_name, 
-               x = 100*n_min100.post/n_total.post),
+               x = 100*n_min100.post/total_sp),
            stat = "identity", alpha = .8) +
-  geom_text(aes(label = paste0("+", (n_min100.post-n_min100.pre)), 
-                x = 100*n_min100.post/n_total.post, 
+  geom_text(data = filter(btg_temp, iconic_taxon_name != "Reptilia"),
+                          aes(label = paste0("+", (n_min100.post-n_min100.pre)), 
+                x = 100*n_min100.post/total_sp, 
                 y = iconic_taxon_name), 
             size = 5,  hjust = -.2, color = "white") +
-  geom_text(aes(label = paste0(n_total.post, " "), 
+  geom_text(data = filter(btg_temp, iconic_taxon_name == "Reptilia"),
+            aes(label = paste0("+", (n_min100.post-n_min100.pre)), 
+                x = 100*n_min100.post/total_sp, 
+                y = iconic_taxon_name), 
+            size = 5,  hjust = +1.1, color = "white") +
+  geom_text(aes(label = paste0(total_sp, " "), 
                 x = 100, 
                 y = iconic_taxon_name), 
             size = 4,  hjust = +1.1, color = "grey40") +
   colorspace::scale_color_discrete_qualitative() +
-  labs(x = "Species (%)", 
+  labs(x = "Taxa (%)", 
        y = "",
        title = "Reached 100 observations") +
   theme(legend.position = "none",
         panel.grid.minor = element_blank())
-ggsave("figures/n_observations/min100_proportionoftotalspecies_labelledn.png", width = 7.94, height = 5.73)
-
-
-# Basic number of species with 100 obs plot ------------------------------------
-
-btg_temp$iconic_taxon_name = factor(btg_temp$iconic_taxon_name,
-                                    levels = btg_temp$iconic_taxon_name[order(btg_temp$diff)])
+ggsave("figures/n_observations/min100_proportionoftotalspecies_labelledn.png", width = 8.1, height = 5.73)
 
 
 ## count number of species that have at least 100 obs now!
-btg_temp$diff |> sum() # 423
+btg_temp$diff |> sum() 
+
 
 # Minimum 10 observations -----------------------------------------------------
 
 # Prepare data for plotting
 btg_temp = filter(btg, !is.na(iconic_taxon_name))
-btg_temp$total_sp = c(574, 54, 1659, 1398, 759, 891, 9064, 19460, 215, 950, 9086, 519, 65)
 btg_temp = btg_temp |>
   mutate("diff" = n_min10.post-n_min10.pre,
          "diffperc" = 100*(n_min10.post-n_min10.pre)/n_total.post) |>
   mutate("Percentage" = paste0(round(diffperc, digits = 0), "%"),
          "Species" = paste0(diff, " sp."))
+
+# manually adding total species counts from inat.ca 'leaves'
+# on june 12, 2026
+btg_temp$total_sp = c(574, 54, 2387, 1398, 759, 891, 9064, 19459, 215, 948, 9087, 519, 56)
+
+# fix and order taxon names
 btg_temp$iconic_taxon_name = gsub("Animalia", "Other Animalia", btg_temp$iconic_taxon_name)
 btg_temp$iconic_taxon_name = factor(btg_temp$iconic_taxon_name,
-                                    levels = btg_temp$iconic_taxon_name[order(btg_temp$n_min10.post/btg_temp$n_total.post)])
+                                    levels = btg_temp$iconic_taxon_name[order(btg_temp$n_min10.post/btg_temp$total_sp)])
 
 
 # Relative gain with n species labels ------------------------------------------
+full.groups = c("Reptilia", "Amphibia", "Mammalia", "Aves")
 
+btg_temp$pre = btg_temp$n_min10.pre/btg_temp$total_sp
+btg_temp$post = btg_temp$n_min10.post/btg_temp$total_sp
+btg_temp$pre[which(btg_temp$pre>1)] <- 1
+btg_temp$post[which(btg_temp$post>1)] <- 1
 ggplot(data = btg_temp, 
        aes(fill = iconic_taxon_name)) +
   geom_bar(aes(y = iconic_taxon_name, 
                x = 100),
            stat = "identity", fill = "black") +
   geom_bar(aes(y = iconic_taxon_name, 
-               x = 100*n_min10.pre/n_total.post),
+               x = 100*pre),
            stat = "identity", alpha = 1) +
   geom_bar(aes(y = iconic_taxon_name, 
-               x = 100*n_min10.post/n_total.post),
+               x = 100*post),
            stat = "identity", alpha = .8) +
-  geom_text(aes(label = paste0("+", (n_min10.post-n_min10.pre)), 
-                x = 100*n_min10.post/n_total.post, 
+  geom_text(data = filter(btg_temp, ! iconic_taxon_name %in% full.groups),
+            aes(label = paste0("+", (n_min10.post-n_min10.pre)), 
+                x = 100*n_min10.post/total_sp, 
                 y = iconic_taxon_name), 
             size = 5,  hjust = -.2, color = "white") +
-  geom_text(aes(label = paste0(n_total.post, ""), 
+  geom_text(data = filter(btg_temp, iconic_taxon_name == "Aves"),
+            aes(label = paste0("+", (n_min10.post-n_min10.pre)), 
+                x = 100*n_min10.post/total_sp, 
+                y = iconic_taxon_name), 
+            size = 5,  hjust = +2.1, color = "white") +
+  geom_text(aes(label = paste0(total_sp, ""), 
                 x = 100, 
                 y = iconic_taxon_name), 
             size = 4,  hjust = +1.1, color = "grey40") +
   colorspace::scale_color_discrete_qualitative() +
-  labs(x = "Species (%)", 
+  labs(x = "Taxa (%)", 
        y = "",
        title = "Reached 10 observations") +
   theme(legend.position = "none",
         panel.grid.minor = element_blank())
-ggsave("figures/n_observations/min10_proportionoftotalspecies_labelledn.png", width = 7, height = 6.21)
-
-
-# Basic number of species with 100 obs plot ------------------------------------
-
-btg_temp$iconic_taxon_name = factor(btg_temp$iconic_taxon_name,
-                                    levels = btg_temp$iconic_taxon_name[order(btg_temp$diff)])
-## count number of species that have at least 100 obs now!
-btg_temp$diff |> sum() # 601
-
-
-# Minimum 30 observations ------------------------------------------------------
-
-# Prepare data for plotting
-btg_temp = filter(btg, !is.na(iconic_taxon_name))
-btg_temp = btg_temp |>
-  mutate("diff" = n_min30.post-n_min30.pre,
-         "diffperc" = 100*(n_min30.post-n_min30.pre)/n_min30.post) |>
-  mutate("Percentage" = paste0(round(diffperc, digits = 0), "%"),
-         "Species" = paste0(diff, " sp."))
-btg_temp$iconic_taxon_name = factor(btg_temp$iconic_taxon_name,
-                                    levels = btg_temp$iconic_taxon_name[order(btg_temp$n_min30.post/btg_temp$n_total.post)])
-
-
-# Relative gain as a percentage ------------------------------------------------
-
-ggplot(data = btg_temp, 
-       aes(fill = iconic_taxon_name)) +
-  geom_bar(aes(y = iconic_taxon_name, 
-               x = 100),
-           stat = "identity", fill = "black") +
-  geom_bar(aes(y = iconic_taxon_name, 
-               x = 100*n_min30.pre/n_total.post),
-           stat = "identity", alpha = 1) +
-  geom_bar(aes(y = iconic_taxon_name, 
-               x = 100*n_min30.post/n_total.post),
-           stat = "identity", alpha = .8) +
-  geom_text(aes(label = paste0("+", round(100*(n_min30.post-n_min30.pre)/n_total.post, digits = 1), "%"), 
-                x = 100*n_min30.post/n_total.post, 
-                y = iconic_taxon_name), 
-            size = 5,  hjust = -.1, color = "white") +
-  geom_text(aes(label = paste0(n_total.post, " sp"), 
-                x = 100, 
-                y = iconic_taxon_name), 
-            size = 4,  hjust = +1.1, color = "grey40") +
-  colorspace::scale_color_discrete_qualitative() +
-  labs(x = "Taxa (%)", 
-       y = "",
-       title = "Reached 30 observations") +
-  theme(legend.position = "none",
-        panel.grid.minor = element_blank())
-ggsave("figures/n_observations/min30_proportionoftotalspecies.png", width = 7, height = 6.21)
-
-
-# Relative gain with n species labels ------------------------------------------
-
-ggplot(data = btg_temp, 
-       aes(fill = iconic_taxon_name)) +
-  geom_bar(aes(y = iconic_taxon_name, 
-               x = 100),
-           stat = "identity", fill = "black") +
-  geom_bar(aes(y = iconic_taxon_name, 
-               x = 100*n_min30.pre/n_total.post),
-           stat = "identity", alpha = 1) +
-  geom_bar(aes(y = iconic_taxon_name, 
-               x = 100*n_min30.post/n_total.post),
-           stat = "identity", alpha = .8) +
-  geom_text(aes(label = paste0("+", (n_min30.post-n_min30.pre)), 
-                x = 100*n_min30.post/n_total.post, 
-                y = iconic_taxon_name), 
-            size = 5,  hjust = -.2, color = "white") +
-  geom_text(aes(label = paste0(n_total.post, " sp"), 
-                x = 100, 
-                y = iconic_taxon_name), 
-            size = 4,  hjust = +1.1, color = "grey40") +
-  colorspace::scale_color_discrete_qualitative() +
-  labs(x = "Taxa (%)", 
-       y = "",
-       title = "Reached 30 observations") +
-  theme(legend.position = "none",
-        panel.grid.minor = element_blank())
-ggsave("figures/n_observations/min30_proportionoftotalspecies_labelledn.png", width = 7, height = 6.21)
+ggsave("figures/n_observations/min10_proportionoftotalspecies_labelledn.png", width = 8.1, height = 5.73)
